524f4d2e1bdc47e857cc2c907b8340358163fc51
Shashwat Pandey <pandeyshashwatptu@gmail.com>
2024-05-13 15:49:35 +0530

    implimented dotenv files

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
@@ -6,6 +9,6 @@ app.get('/home',(req,res)=>{
     res.json({message : 'OK'});
 })
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Server is up on port number :- 3000");
 });
